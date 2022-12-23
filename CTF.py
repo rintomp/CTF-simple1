b984ac0a777eed9ffb16fcd3625eecfa8a4f5f66
Rinto M Prince <89894914+rintomp@users.noreply.github.com>
2022-12-23 11:03:31 +0530

    Add files via upload

---
+++ CTF.py
@@ -0,0 +1,33 @@
+n = input("Enter answer : ")
+if n == ("start"):
+    print("https://drive.google.com/file/d/1SbGNKAVUNzmixL6iSM_pWG0WtJeFIUUC/view?usp=share_link")
+elif n == ("rm"):
+    print("https://drive.google.com/file/d/1gYtBwSyGNKvZUFR99ZMnWCR4-1_vFDFx/view?usp=share_link")
+elif n == ("mv"):
+    print("https://drive.google.com/file/d/1SXw7I-gj9DEj_Q65EVeeke0YEm7LRBdt/view?usp=share_link")
+elif n == ("copy running-config startup-config"):
+    print("https://drive.google.com/file/d/1BQC-kDlb7L4_LZtoMTHPpujFed23nMtf/view?usp=share_link")
+elif n == ("icmp"):
+    print("https://drive.google.com/file/d/1J5LVSeMcOZM50WGKsA7Zl5RSzNqda3kS/view?usp=share_link")
+elif n == ("127.0.0.1"):
+    print("https://drive.google.com/file/d/1jTALct3_ufNN54sPSQzL_20Q9NV-tXUv/view?usp=share_link")
+elif n == ("data link layer"):
+    print("https://drive.google.com/file/d/1NjI3Y1xwr7dgAyyXt3mvTavUVE7QSCy8/view?usp=share_link")
+elif n == ("ssh admin@10.10.10.1"):
+    print("https://drive.google.com/file/d/1dVfGM9TyuNbBVnzcChPm0_tACvjGfK8e/view?usp=share_link")
+elif n == ("ospf"):
+    print("https://drive.google.com/file/d/18tRhaauUUb6g1v-oVB4qryJqDLuJ3bpE/view?usp=share_link")
+elif n == ("45.80.182.28"):
+    print("https://drive.google.com/file/d/1LetGEeVZ24dYQ-zwFORNmepc7hSLpaKW/view?usp=share_link")
+elif n == ("signals"):
+    print("https://drive.google.com/file/d/1McCDXr07cDM_IaZiYTFMRK5GHhvxyqCi/view?usp=share_link")
+elif n == ("ls -a"):
+    print("https://drive.google.com/file/d/13ica9TxGl4A_swG0ckRi7UncwxE21xju/view?usp=share_link")
+elif n == ("enhanced interior gateway routing protocol"):
+    print("https://drive.google.com/file/d/1aVgUfF6ADoaIiBy59VhHWxNbyvKPEkhv/view?usp=share_link")
+elif n == ("ack"):
+    print("https://drive.google.com/file/d/1vfSvN5Uu2srGapjCqTdfDLPMw7PL9Cij/view?usp=share_link")
+elif n == ("no ip domain-lookup"):
+    print("https://drive.google.com/file/d/1RYpW9SnAuyo06x3RHThbJtM2y9OrGRCR/view?usp=share_link")
+elif n == ("ip route 0.0.0.0 0.0.0.0"):
+    print("https://drive.google.com/file/d/15vJA5lKCxPlzSgoiuNkADULdyZN9GSRG/view?usp=share_link")
